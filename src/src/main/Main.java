102d774d8bd8c0f43ad618c593dd7402ca4b89d6
Student <Student@host.docker.internal>
2025-10-27 15:48:24 +0100

    1

--- src/src/main/Main.java
+++ src/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import gui.SportvereinControl;
 import gui.SportvereineAnwendungssystem;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -8,7 +9,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		new SportvereineAnwendungssystem(primaryStage);
+		new SportvereinControl(primaryStage);
 	}	
 	
 	public static void main(String[] args){
